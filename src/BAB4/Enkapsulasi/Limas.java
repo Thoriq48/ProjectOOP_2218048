e73dac6b5eb8dc4053585c085853ca3686fc6144
Thoriq48 <153270940+Thoriq48@users.noreply.github.com>
2023-12-17 19:24:55 +0700

    Add files via upload

---
+++ src/BAB4/Enkapsulasi/Limas.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BAB4.Enkapsulasi;
+
+/**
+ *
+ * @author Aharfi
+ */
+public class Limas {
+    double alas,tinggiLimas,volume;
+    double volume(){
+        volume = (0.333*alas*alas*tinggiLimas);
+        return volume;
+    }
+}
